b360048bbe04b4ef15465463d8fb4b7211b2dd1c
Jasmin <122053640+jahernandez16@users.noreply.github.com>
2023-02-21 10:40:17 -0800

    added jumping and running effects to the player

--- My project/Assets/Scripts/MoveLeft.cs
+++ My project/Assets/Scripts/MoveLeft.cs
@@ -6,7 +6,7 @@ public class MoveLeft : MonoBehaviour
 {
     private float speed = 30;
     private PlayerController playerControllerScript;
-    private float leftBound = 15;
+    private float leftBound = -15;
 
     // Start is called before the first frame update
     void Start()
